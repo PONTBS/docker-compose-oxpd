120be455562adec60c1a1f5881488e0448522773
Timm Bangma <bangma@hp.com>
2019-03-07 10:31:41 -0500

    Added docker compose up to build.

--- build.sh
+++ build.sh
@@ -1,20 +1,23 @@
 webappdir='/home/timm/git/roam-embedded'
 proxydir='/home/timm/git/roam-embedded-proxy/GatewayApp'
 
-echo -e "\e[1mBuilding Webapp Image\e[0m"
+echo -e "\e[1m\n === Building Webapp Image === \e[0m"
 docker build \
   -t local/oxpd-web \
   -f webapp.df \
   $webappdir
 
-echo -e "\e[1mRunning Maven Build For Proxy\e[0m"
+echo -e "\e[1m\n === Running Maven Build For Proxy === \e[0m"
 prevdir=$(pwd)
 cd $proxydir
 mvn clean install -DskipTests
 cd $prevdir
 
-echo -e "\e[1mBuilding Proxy Image\e[0m"
+echo -e "\e[1m\n === Building Proxy Image === \e[0m"
 docker build \
   -t local/oxpd-proxy \
   -f proxy.df \
   $proxydir
+
+echo -e "\e[1m\n === Starting Containers === \e[0m"
+docker-compose up
